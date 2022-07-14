c702f9737e1d9540ec731a68fd25a28270f33550
Ram-Santosh <ram.santosh@techmojo.in>
2022-07-14 18:52:45 +0530

    Activity 2 submit left

--- src/Activity-2.js
+++ src/Activity-2.js
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 
 export default function UnControlledForm () {
@@ -8,36 +8,122 @@ export default function UnControlledForm () {
     var securityQuestion = useRef("");
     var securityAnswer = useRef("");
     var bio = useRef("");
-
-    var handleFormInput = (e) => {
-        var val = e.target.value;
-        var name = e.target.name;
-
-        [name].current.value = val;
-    }
+    var [errors, setErrors] = useState({
+        firstNameError: "",
+        lastNameError:"",
+        bioError:""
+    })
 
     var clearForm = () => {
-
+        firstName.current.value= "";
+        lastName.current.value= "";
+        email.current.value= "";
+        securityQuestion.current.value= "";
+        securityAnswer.current.value= "";
+        bio.current.value= "";
+        document.getElementsByName("firstName")[0].value = "";
+        document.getElementsByName("lastName")[0].value = "";
+        document.getElementsByName("email")[0].value = "";
+        document.getElementsByName("securityQuestion")[0].value = "Select an option";
+        document.getElementsByName("securityAnswer")[0].value = "";
+        document.getElementsByName("bio")[0].value = "";
     }
+  
+    var handleSubmit = (e) => {
+        var validFirstName = false;
+        var validLastName = false;
+        var validBio = false;
+        var validEmail = false;
+        var validSecQues = false;
+        var validSecAns = false;
+        e.preventDefault();
+        
+        //validate firstname
+        if ((firstName.current.value.length < 3 || firstName.current.value.length > 15) && firstName.current.value.length > 0){
+            setErrors({...errors,firstNameError:"Length of the first name should be between 3 to 15 letters."})
+        }
+        else if (firstName.current.value.length > 0 && !firstName.current.value.match(/^[a-zA-Z]+$/)){
+            setErrors({...errors,firstNameError:"Name should only contain letters."})              
+        }
+        else{
+            setErrors({...errors,firstNameError:""})  
+            if (firstName.current.value.length > 0 ){
+                validFirstName = true;
+            }
+        }
 
-    var handleSubmit = () => {
+        //validate lastname
+        if ((lastName.current.value.length < 3 || lastName.current.value.length > 15) && lastName.current.value.length > 0){
+            setErrors({...errors,lastNameError:"Length of the last name should be between 3 to 15 letters."})
+        }
+        else if (lastName.current.value.length > 0 && !lastName.current.value.match(/^[a-zA-Z]+$/)){
+            setErrors({...errors,lastNameError:"Name should only contain letters."})              
+        }
+        else{
+            setErrors({...errors,lastNameError:""})
+            if (lastName.current.value.length > 0 ){
+                validLastName = true;
+            }
+        }
 
+        // Validate bio
+        if (bio.current.value.length < 15 && bio.current.value.length > 0){
+            setErrors({...errors,bioError:"Length of the bio should be greater than 15 letters."})
+        }
+        else{
+            setErrors({...errors,bioError:""})
+            if (bio.current.value.length > 0 ){
+                validBio = true;
+            }
+        }
+
+        // Validate email
+        if (email.current.value.length > 0 ){
+            validEmail = true;
+        }
+
+
+        // Validate secret question
+        if (securityQuestion.current.value !== "Select an option" ){
+            validSecQues = true;
+        }
+
+
+        // Validate secret answer
+        if (securityAnswer.current.value.length > 0 ){
+            validSecAns = true;
+        }
+
+        console.log(errors)
+        if (validBio && validEmail && validFirstName && validLastName && validSecAns && validSecQues){
+            var form = {"email":email.current.value, 
+                        "bio":bio.current.value, 
+                        "first name":firstName.current.value,
+                        "last name":lastName.current.value,
+                        "Secret question":securityQuestion.current.value,
+                        "Secret answer":securityAnswer.current.value}
+
+            console.log(form)
+        }
     }
 
     return (
         <>
         <form onSubmit={handleSubmit}>
-            <input type="text" onKeyUp={handleFormInput} name="firstName" placeholder="Enter your First name"></input><br/><br/>
-            <input type="text" onKeyUp={handleFormInput} name="lastName" placeholder="Enter your Last name"></input><br/><br/>
-            <input type="email" onKeyUp={handleFormInput} name="email" placeholder="Enter your Email"></input><br/><br/>
-            <select name="securityQuestion" onChange={handleFormInput} required>
+            <input type="text" ref={firstName} name="firstName" placeholder="Enter your First name"></input><br/>
+            {errors.firstNameError}<br/>
+            <input type="text" ref={lastName} name="lastName" placeholder="Enter your Last name"></input><br/>
+            {errors.lastNameError}<br/>
+            <input type="email" ref={email} name="email" placeholder="Enter your Email"></input><br/>
+            <select name="securityQuestion" ref={securityQuestion} required>
                 <option value="Select an option">Select an option</option>
                 <option value="What is your mother's maiden name?">What is your mother's maiden name?</option>
                 <option value="What was the name of your first pet?">What was the name of your first pet?</option>
                 <option value="What was the name of your first school?">What was the name of your first school?</option>
             </select><br/><br/>
-            <input type="text" onKeyUp={handleFormInput} name="securityAnswer" placeholder="Enter your answer" required></input><br/><br/>
-            <textarea type="text" onKeyUp={handleFormInput} name="bio" placeholder="Enter your Bio" rows="5" cols="100"></textarea><br/>
+            <input type="text" ref={securityAnswer} name="securityAnswer" placeholder="Enter your answer" required></input><br/><br/>
+            <textarea type="text" ref={bio} name="bio" placeholder="Enter your Bio" rows="5" cols="100"></textarea><br/>
+            {errors.bioError}<br/>
             <button type="submit">Submit</button>&nbsp;
             <button onClick={clearForm}>Clear</button>
         </form>
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from './Activity-1';
+// import Form from './Activity-1';
 import reportWebVitals from './reportWebVitals';
-// import UnControlledForm from './Activity-2';
+import UnControlledForm from './Activity-2';
 
 //Activity 1
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Form />
-  </React.StrictMode>
-);
-
-//Activity 2
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
-//     <UnControlledForm />
+//     <Form />
 //   </React.StrictMode>
 // );
+
+//Activity 2
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UnControlledForm />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
